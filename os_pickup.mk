bb2bb00f475b3cc2001b000ebd9cc5fe54fd1125
Bruno Martins <bgcngm@gmail.com>
2019-02-17 14:35:59 +0000

    os_pickup: Guard to avoid conflicts with CAF repos
     * This makefile ends up in hardware/qcom/sdm845 and all the
       makefiles under that folder should only be called by AOSP
       devices.
    
    Change-Id: Ice7530bc01de89e2c1a27c4442eedc14d112ebe9
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- os_pickup.mk
+++ os_pickup.mk
@@ -1,4 +1,6 @@
+ifneq ($(BOARD_USES_QCOM_HARDWARE),true)
 ifneq ($(filter sdm845 sdm710,$(TARGET_BOARD_PLATFORM)),)
 LOCAL_PATH := $(call my-dir)
 include $(call first-makefiles-under,$(LOCAL_PATH))
+endif
 endif
